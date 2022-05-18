eb105a0e5572281d2580058db8584bcf2dc89f61
Maksym Herasymov <maxgerasimov159753@gmail.com>
2022-05-18 16:02:26 +0300

    revers str

---
+++ home work1/reverse_string.js
@@ -0,0 +1,6 @@
+function solution(str){
+    return str.split("").reverse().join("");
+
+}
+//git config -- user.email "maxgerasimov159753@gmail.com"
+// //git config -- user.name "Maksym Herasymov"
